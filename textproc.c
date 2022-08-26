1f967f8e93e8a537fda5f9a8722dfc7545e9f3dd
Corin Dobrica <corin.dobrica@windriver.com>
2022-08-26 14:23:05 +0300

    Added input file and changed to O(n)

--- textproc.c
+++ textproc.c
@@ -2,36 +2,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+const int MSIZE = 1000;
 
-int wpercent(const char *text1, const char *text2) {
-  int counter = 0;
-  int length = 0;
+const char *readFile(const char *filename) {
+  int c, length = 0;
+  char *buffer = 0;
+  
+  FILE *file1;
+  file1 = fopen(filename, "r");
+
+  if (file1 == NULL) {
+    printf("Error, couldn't open file!");
+    exit(EXIT_FAILURE);
+  }
+
+  if (file1) {
+    fseek(file1, 0, SEEK_END);
+    length = ftell(file1);
+
+    fseek(file1, 0, SEEK_SET);
+    buffer = malloc(length);
+
+    if (buffer) {
+      fread(buffer, 1, length, file1);
+    }
+
+    fclose(file1);
+  }
+
+  return buffer;
+}
+
+float wpercent(const char *text1, const char *text2) {
+  int counter = 0, length = 0;
   float result;
 
   size_t size1 = strlen(text1);
   size_t size2 = strlen(text2);
 
-  for (int i = 0; i < size1; i++)
-    for (int j = 0; j < size2; j++) {
-      if (isalnum(text1[i]) && isalnum(text2[j]) && text1[i] == text2[j]) {
-        counter++;
-      }
+  for (int i = 0; i < size1; i++) {
+    if ((isalnum(text1[i]) || ispunct(text1[i])) &&
+        strchr(text2, text1[i]) != NULL) {
+      counter++;
     }
+  }
 
   for (int i = 0; i < size1; i++) {
-    if (isalnum(text1[i]))
+    if (isalnum(text1[i]) || ispunct(text1[i]))
       length++;
   }
-  
+
   return result = (float)counter / length * 100;
 }
 
 int main() {
 
-  const char *t1 = "abc   def";
-  const char *t2 = "a c";
-  float result;
+  const char *t1 = readFile("text1.txt");
+  const char *t2 = "abcdefghijklmnopqrstuvwxyz";
 
-  result = wpercent(t1, t2);
-  printf("%f percents", result);
+  float result = wpercent(t1, t2);
+  printf("%.2f percents", result);
 }
