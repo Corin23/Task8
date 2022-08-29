fc826032a83d75eabb54f61a480717ad81271e23
Corin23 <dobricacorin5@yahoo.com>
2022-08-29 14:30:36 +0300

    update

--- txtproc.c
+++ txtproc.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 const int MSIZE = 100;
+const int ASCH = 128;
 
 const char *readFile(const char *filename) {
   int c, length = 0;
   long fsize = MSIZE;
-  char *buffer = 0;
+  char *buffer;
 
   FILE *file1;
   file1 = fopen(filename, "r");
@@ -42,16 +43,24 @@ const char *readFile(const char *filename) {
     fclose(file1);
   }
   return buffer;
+  
 }
 
 float wpercent(const char *text1, const char *text2) {
   int counter = 0, length = 0;
+  int txt1arr[128] = {0}, txt2arr[128] = {0};
   float result;
 
   for (int i = 0; text1[i] != '\0'; i++) {
-    if ((isalnum(text1[i]) || ispunct(text1[i]) || isspace(text1[i])) &&
-        strchr(text2, text1[i]) != NULL) {
-      counter++;
+    ++(txt1arr[(int)text1[i]]);
+  }
+  for (int i = 0; text2[i] != '\0'; i++){
+    ++(txt2arr[(int)text2[i]]);
+  }
+  for(int i = 0; i< ASCH; i++){
+    if((txt1arr[i] && txt2arr[i]) != 0)
+    {
+      counter += txt1arr[i];
     }
   }
 
@@ -66,7 +75,7 @@ float wpercent(const char *text1, const char *text2) {
 int main() {
 
   const char *t1 = readFile("text1.txt");
-  const char *t2 = "abcdefghijklmnopqrstuvwxyz ";
+  const char *t2 = "a b";
   float result = wpercent(t1, t2);
   printf("%.2f percents", result);
 }
